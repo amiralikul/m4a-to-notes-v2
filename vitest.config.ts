ae0acf95f26e7f0fdee57b55e3d826dbd4b29708
amiralkul <amiralikulov@Amirs-MacBook-Pro-2.local>
2026-02-26 18:22:42 +0200

    fix test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
 import { resolve } from "node:path";
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
 		globals: true,
 		environment: "node",
+		exclude: [...configDefaults.exclude, "e2e/**"],
 	},
 	resolve: {
 		alias: {
